44715580e09f8ef3258a0d35f9f1fb1672b44e6a
Dhruvit Raithatha <dhruvit.raithatha@gmail.com>
2021-11-22 16:36:44 +0530

    Changes for testing

--- src/extension.ts
+++ src/extension.ts
@@ -13,13 +13,32 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('codelens-test.helloWorld', () => {
+	const commandId = 'codelens-test.helloWorld';
+
+	context.subscriptions.push(vscode.commands.registerCommand(commandId, () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from codelens-test!');
-	});
+	}));
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.languages.registerCodeLensProvider(
+			'*',
+			{
+				provideCodeLenses: (document, token) => [
+					new vscode.CodeLens(
+						new vscode.Range(2, 1, 2, 2),
+						{
+
+							title: 'Test Regex...',
+							command: commandId,
+							arguments: ['Hello']
+						}
+					)
+				]
+			}
+		)
+	);
 }
 
 // this method is called when your extension is deactivated
